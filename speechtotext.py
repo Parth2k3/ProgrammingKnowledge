08ab0c10a750f82766e6a789ef08df6ca01fbe3a
Parth <parth.nangroo@gmail.com>
2025-01-15 21:53:44 +0530

    update

--- speechtotext.py
+++ speechtotext.py
@@ -1,31 +1,34 @@
 from google.cloud import speech_v1 as speech
+import os
 from pydub import AudioSegment
+import wave
 
-# Load the audio file
-audio = AudioSegment.from_file("sample_audio.wav")
+with wave.open("sample_audio.wav", "rb") as wav_file:
+    sample_rate = wav_file.getframerate()
+    print(f"Sample rate: {sample_rate}")
 
-# Convert to mono
-audio = audio.set_channels(1)
+client = speech.SpeechClient.from_service_account_file('gcp_key.json')
 
-# Export the converted audio
-audio.export("audio_sample_mono.wav", format="wav")
+def convert_to_mono(ipnut_file, output_file):
+    audio = AudioSegment.from_file(ipnut_file)
+    audio = audio.set_channels(1)
+    audio.export(output_file, format="wav")
+    print('Converted to mono')
 
-client = speech.SpeechClient.from_service_account_file("gcp_key.json")
+def transcribe_audio(input_file):
+    with open(input_file, 'rb') as audio_file:
+        content = audio_file.read()
+        audio = speech.RecognitionAudio(content=content)
+        config = speech.RecognitionConfig(
+            encoding = speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz = sample_rate,
+            language_code = 'en-US'
+        )
+        response = client.recognize(config=config, audio=audio)
+        for result in response.results:
+            print(f'Transcript: {result.alternatives[0].transcript}')
+            print(f'Confidence: {result.alternatives[0].confidence}')
+    return response
 
-with open("audio_sample_mono.wav", "rb") as audio_file:
-    content = audio_file.read()
-
-# Configure audio and recognition settings
-audio = speech.RecognitionAudio(content=content)
-config = speech.RecognitionConfig(
-    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=44100,
-    language_code="en-US"
-)
-
-# Perform recognition
-response = client.recognize(config=config, audio=audio)
-
-# Print the transcription
-for result in response.results:
-    print("Transcript:", result.alternatives[0].transcript)
+convert_to_mono("sample_audio.wav", "audio_sample_mono.wav")
+transcribe_audio("audio_sample_mono.wav")
